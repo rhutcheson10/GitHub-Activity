66ac28011def9f732a8c830354eb5bebb3945ad5
rhutcheson10 <41714048+rhutcheson10@users.noreply.github.com>
2019-07-26 09:36:20 +1200

    Testing finished

--- Bank2/BankAccount.cs
+++ Bank2/BankAccount.cs
@@ -9,6 +9,8 @@ namespace BankAccountNS
     {
         private readonly string m_customerName;
         private double m_balance;
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
 
         private BankAccount() { }
 
@@ -32,15 +34,15 @@ namespace BankAccountNS
         {
             if (amount > m_balance)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
 
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
 
-            m_balance += amount; // intentionally incorrect code
+            m_balance -= amount; // fixed code code
         }
 
         public void Credit(double amount)
